71dab76fd342873a90a5b04513cf70d0e89a37f8
Jameel <47351791+jameelappsintegra@users.noreply.github.com>
2021-12-29 11:54:12 +0400

    Add files via upload

---
+++ components/common/card/index.js
@@ -0,0 +1,30 @@
+const Card = (props) => {
+  const { nameColor, cardBg } = props;
+  return (
+    <div className="p-4">
+      <figure className={`${cardBg} md:flex  rounded-xl p-8 md:p-0 dark:bg-gray-800`}>
+        <img
+          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+          src="https://tailwindcss.com/_next/static/media/sarah-dayan.a8ff3f1095a58085a82e3bb6aab12eb2.jpg"
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I have seen scale on large teams. It’s easy to
+              customize, adapts to any design, and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className={`${nameColor} dark:text-sky-400`}>Sarah Dayan</div>
+            <div className="text-gray-700 dark:text-gray-500">Staff Engineer, Algolia</div>
+          </figcaption>
+        </div>
+      </figure>
+    </div>
+  );
+};
+
+export default Card;
