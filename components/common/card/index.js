ea89e005ade7d397e1332fbadafaec2afda9d3b5
jameelappsintegra <mail4jameelmca@gmail.com>
2022-01-07 11:03:41 +0400

    changes

--- components/common/card/index.js
+++ components/common/card/index.js
@@ -1,35 +1,68 @@
 const Card = (props) => {
-  const { cardImg, nameText, nameColor, cardBg } = props;
+  const { cardImg, nameText, nameColor, cardBg, cardType, title, svg, content } = props;
   return (
-    <div className="p-4">
-      <figure className={`${cardBg} md:flex  rounded-xl p-8 md:p-0 dark:bg-gray-800`}>
-        <img
-          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-          src={cardImg}
-          alt=""
-          width="384"
-          height="512"
-        />
-        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-          <blockquote>
-            <p className="text-lg font-medium">
-              “Tailwind CSS is the only framework that I have seen scale on large teams. It’s easy to
-              customize, adapts to any design, and the build size is tiny.”
-            </p>
-          </blockquote>
-          <figcaption className="font-medium">
-            <div className={`${nameColor} dark:text-sky-400`}>{nameText}</div>
-            <div className="text-gray-700 dark:text-gray-500">Staff Engineer, Algolia</div>
-          </figcaption>
-          <button
-            type="button"
-            className="bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg"
+    <>
+      {cardType === 'linedCard' ? (
+        <div class="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
+          {svg && <span class="inline-block text-blue-500 dark:text-blue-400">{svg}</span>}
+          {title && (
+            <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-blue-400">{title}</h1>
+          )}
+          {content && <p class="text-gray-500 dark:text-blue-700">{content}</p>}
+          <a
+            href="#"
+            class="inline-flex p-2 text-blue-500 capitalize transition-colors duration-200 transform bg-blue-100 rounded-full dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500"
           >
-            Check availability
-          </button>
+            {
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+            }
+          </a>
         </div>
-      </figure>
-    </div>
+      ) : (
+        <div className="pb-4">
+          <figure className={`${cardBg} md:flex  rounded-xl p-8 md:p-0 `}>
+            <img
+              className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+              src={cardImg}
+              alt=""
+              width="384"
+              height="512"
+            />
+            <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+              <blockquote>
+                <p className="text-lg font-medium">
+                  “Tailwind CSS is the only framework that I have seen scale on large teams. It’s easy to
+                  customize, adapts to any design, and the build size is tiny.”
+                </p>
+              </blockquote>
+              <figcaption className="font-medium">
+                <div className={`${nameColor}`}>{nameText}</div>
+                <div className="text-gray-700">Staff Engineer, Algolia</div>
+              </figcaption>
+              <button
+                type="button"
+                className="bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg"
+              >
+                Check availability
+              </button>
+            </div>
+          </figure>
+        </div>
+      )}
+    </>
   );
 };
 
---
+++ components/templates/exploreSection/index.js
@@ -0,0 +1,45 @@
+import { CleanDesignIcon, CustomIcon, ExploreIcon } from '../../../utils/icon';
+import Card from '../../common/card';
+
+const cardExplore = [
+  {
+    title: 'Elegant Dark Mode',
+    icon: <ExploreIcon />,
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet',
+  },
+  {
+    title: 'Easy To Customiztions',
+    icon: <CustomIcon />,
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet',
+  },
+  {
+    title: 'Simple & Clean Designs',
+    icon: <CleanDesignIcon />,
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet',
+  },
+];
+
+const ExploreSection = () => {
+  return (
+    <>
+      <h1 class="text-3xl font-semibold text-blue-800 capitalize lg:text-4xl dark:text-blue-500">
+        explore our <br /> awesome <span class="underline decoration-blue-500">Components</span>
+      </h1>
+
+      <p class="mt-4 text-gray-500 xl:mt-6 dark:text-blue-700">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quam voluptatibus
+      </p>
+      <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
+        {cardExplore &&
+          cardExplore.map((item, index) => (
+            <Card cardType="linedCard" content={item?.content} title={item?.title} svg={item?.icon} />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default ExploreSection;
